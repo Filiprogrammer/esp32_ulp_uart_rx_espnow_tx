c1258318982883da6bb9e2f003d0c56206870a3a
Filiprogrammer <44641787+Filiprogrammer@users.noreply.github.com>
2025-05-23 17:23:48 +0200

    Add ESP-NOW sending

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include <esp_attr.h>
+#include <esp_now.h>
 #include <esp_sleep.h>
+#include <esp_wifi.h>
 #include <esp32/ulp.h>
 #include <driver/rtc_io.h>
+#include <nvs.h>
+#include <nvs_flash.h>
 #include <sdkconfig.h>
 #include <soc/rtc_io_reg.h>
 #include <string.h>
@@ -9,11 +13,80 @@
 
 RTC_DATA_ATTR bool ulp_running = false;
 RTC_DATA_ATTR uint8_t rx_buffer[246];
+RTC_DATA_ATTR uint32_t sequence_number = 0;
 #define RTC_MEM_ULP_PROGRAM_ADDRESS 0x100
 #define RTC_MEM_RECEIVE_BUFFER_ADDRESS 0x300
 // BAUD_RATE 2400
 // ULP_CLOCK_SPEED 8000000
 // Should be 3333.33 clock cycles per bit
+
+const uint8_t receiver_address[6] = RECEIVER_ADDRESS;
+
+typedef struct __attribute__((packed)) DataPacket {
+    uint32_t sequence_number;
+    uint8_t data[246];
+} DataPacket;
+
+bool send_success;
+SemaphoreHandle_t send_success_semaphore = NULL;
+
+void on_data_sent(const uint8_t* mac_addr, esp_now_send_status_t status) {
+    if (status == ESP_NOW_SEND_SUCCESS) {
+        puts("Delivery success");
+        send_success = true;
+    } else {
+        puts("Delivery failed");
+        send_success = false;
+    }
+
+    xSemaphoreGive(send_success_semaphore);
+}
+
+bool send_data_packet(const DataPacket packet_to_send) {
+    printf("Sending Packet - Seq: %lu, Data: ", packet_to_send.sequence_number);
+    for (uint8_t i = 0; i < 246; i++) {
+        printf("%02X", packet_to_send.data[i]);
+    }
+    putchar('\n');
+
+    if (esp_now_send(receiver_address, (uint8_t*)&packet_to_send, sizeof(DataPacket)) != ESP_OK) {
+        puts("ESP-NOW send command failed immediately");
+        return false;
+    }
+
+    xSemaphoreTake(send_success_semaphore, portMAX_DELAY);
+    return send_success;
+}
+
+void setup_esp_now() {
+    const wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_ERROR_CHECK(esp_now_init());
+
+    // Register the send callback
+    esp_now_register_send_cb(on_data_sent);
+
+    // Add receiver as a peer
+    esp_now_peer_info_t peer_info;
+    memset(&peer_info, 0, sizeof(peer_info));
+    memcpy(peer_info.peer_addr, receiver_address, 6);
+    peer_info.channel = 0;
+    peer_info.encrypt = false;
+
+    ESP_ERROR_CHECK(esp_now_add_peer(&peer_info));
+    puts("Sender ready");
+}
 
 void setupULP() {
     gpio_config_t pin_config = {
@@ -109,20 +182,31 @@ void setupULP() {
 }
 
 void app_main() {
-    if (!ulp_running) {
+    bool is_first_run = !ulp_running;
+
+    if (is_first_run) {
         puts("Setting up ULP...");
         setupULP();
         puts("ULP setup done");
     }
 
-    esp_sleep_enable_ulp_wakeup();
-
-    for (uint32_t i = 0; i < 246; ++i) {
-        uint8_t c = rx_buffer[i];
-        putchar((char)c);
+    if (!is_first_run) {
+        send_success_semaphore = xSemaphoreCreateBinary();
+        DataPacket packet_to_send;
+        packet_to_send.sequence_number = sequence_number;
+        memcpy(packet_to_send.data, rx_buffer, sizeof(rx_buffer));
+        setup_esp_now();
+        for (uint8_t retry_count = 0; retry_count <= MAX_RETRANSMITS; retry_count++) {
+            if (send_data_packet(packet_to_send)) {
+                // Successfully delivered, no need to retry anymore
+                break;
+            }
+        }
+        esp_wifi_stop();
+        sequence_number++;
     }
-    fflush(stdout);
 
+    esp_sleep_enable_ulp_wakeup();
     esp_deep_sleep_start();
 }
 
